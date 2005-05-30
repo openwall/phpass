5491bdb2ec926190ee844cddaa63c260edacad8e
Solar Designer <solar@openwall.com>
2005-05-31 03:24:02 +0400

    Initial import of phpass 0.0.
    CVS-ID: c/Makefile 1.1
    CVS-ID: c/crypt_private.c 1.1
    CVS-ID: PasswordHash.php 1.1
    CVS-ID: test.php 1.1

---
+++ test.php
@@ -0,0 +1,34 @@
+<?
+#
+# This is a test program for the portable PHP password hashing framework.
+#
+# Written by Solar Designer and placed in the public domain.
+# See PasswordHash.php for more information.
+#
+
+require "PasswordHash.php";
+
+# Try to use stronger but system-specific hashes, with a possible fallback to
+# the weaker portable hashes.
+$t_hasher = new PasswordHash(8, FALSE);
+
+$pw1 = "test12345";
+$pw2 = "test12346";
+$hash = $t_hasher->HashPassword($pw1);
+
+print $hash . "\n";
+print $t_hasher->CheckPassword($pw1, $hash) . "\n"; # prints 1
+print $t_hasher->CheckPassword($pw2, $hash) . "\n"; # prints 0 or nothing
+
+unset($t_hasher);
+
+# Force the use of weaker portable hashes.
+$t_hasher = new PasswordHash(8, TRUE);
+
+$hash = $t_hasher->HashPassword($pw1);
+
+print $hash . "\n";
+print $t_hasher->CheckPassword($pw1, $hash) . "\n"; # prints 1
+print $t_hasher->CheckPassword($pw2, $hash) . "\n"; # prints 0 or nothing
+
+?>
