673f6272593fe76115cf48037d85e546ed38c16e
Solar Designer <solar@openwall.com>
2006-09-08 05:40:12 +0400

    Re-worked the test program to report whether all of the tests have passed.
    CVS-ID: test.php 1.4

--- test.php
+++ test.php
@@ -8,35 +8,63 @@
 
 require "PasswordHash.php";
 
+$ok = 0;
+
 # Try to use stronger but system-specific hashes, with a possible fallback to
 # the weaker portable hashes.
 $t_hasher = new PasswordHash(8, FALSE);
 
-$pw1 = "test12345";
-$pw2 = "test12346";
-$hash = $t_hasher->HashPassword($pw1);
+$correct = "test12345";
+$hash = $t_hasher->HashPassword($correct);
 
-print $hash . "\n";
-print $t_hasher->CheckPassword($pw1, $hash) . "\n"; # prints 1
-print $t_hasher->CheckPassword($pw2, $hash) . "\n"; # prints 0 or nothing
+print "Hash: " . $hash . "\n";
+
+$check = $t_hasher->CheckPassword($correct, $hash);
+if ($check) $ok++;
+print "Check correct: '" . $check . "' (should be '1')\n";
+
+$wrong = "test12346";
+$check = $t_hasher->CheckPassword($wrong, $hash);
+if (!$check) $ok++;
+print "Check wrong: '" . $check . "' (should be '0' or '')\n";
 
 unset($t_hasher);
 
 # Force the use of weaker portable hashes.
 $t_hasher = new PasswordHash(8, TRUE);
 
-$hash = $t_hasher->HashPassword($pw1);
+$hash = $t_hasher->HashPassword($correct);
 
-print $hash . "\n";
-print $t_hasher->CheckPassword($pw1, $hash) . "\n"; # prints 1
-print $t_hasher->CheckPassword($pw2, $hash) . "\n"; # prints 0 or nothing
+print "Hash: " . $hash . "\n";
 
-# A correct hash for "test12345".
-# Please note the use of single quotes to ensure that the dollar signs
-# will be interpreted literally.
+$check = $t_hasher->CheckPassword($correct, $hash);
+if ($check) $ok++;
+print "Check correct: '" . $check . "' (should be '1')\n";
+
+$check = $t_hasher->CheckPassword($wrong, $hash);
+if (!$check) $ok++;
+print "Check wrong: '" . $check . "' (should be '0' or '')\n";
+
+# A correct portable hash for "test12345".
+# Please note the use of single quotes to ensure that the dollar signs will
+# be interpreted literally.  Of course, a real application making use of the
+# framework won't store password hashes within a PHP source file anyway.
+# We only do this for testing.
 $hash = '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0';
 
-print $t_hasher->CheckPassword($pw1, $hash) . "\n"; # prints 1
-print $t_hasher->CheckPassword($pw2, $hash) . "\n"; # prints 0 or nothing
+print "Hash: " . $hash . "\n";
+
+$check = $t_hasher->CheckPassword($correct, $hash);
+if ($check) $ok++;
+print "Check correct: '" . $check . "' (should be '1')\n";
+
+$check = $t_hasher->CheckPassword($wrong, $hash);
+if (!$check) $ok++;
+print "Check wrong: '" . $check . "' (should be '0' or '')\n";
+
+if ($ok == 6)
+	print "All tests have PASSED\n";
+else
+	print "Some tests have FAILED\n";
 
 ?>
